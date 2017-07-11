b43026dec3ec9d6b06571d5b8f2d0a9b7593bdb0
Oleg Filimonov <themraction@gmail.com>
2017-07-11 21:32:54 +0300

    setup gradle

--- app/src/main/java/com/olegfilimonov/gifer/MainActivity.java
+++ app/src/main/java/com/olegfilimonov/gifer/MainActivity.java
@@ -3,6 +3,8 @@ package com.olegfilimonov.gifer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.olgefilimonov.gifer.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
